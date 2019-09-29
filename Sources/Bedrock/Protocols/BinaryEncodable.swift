44dee0b98048a7aec35216e2e6b13f90d85d3080
Pumper Knickle <pumperknickle@protonmail.com>
2019-09-29 10:00:19 -0700

    make binary encodable equatable

--- Sources/Bedrock/Protocols/BinaryEncodable.swift
+++ Sources/Bedrock/Protocols/BinaryEncodable.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-public protocol BinaryEncodable {
+public protocol BinaryEncodable: Equatable {
     func toBoolArray() -> [Bool]
     init?(raw: [Bool])
 }
@@ -8,5 +8,11 @@ public protocol BinaryEncodable {
 public extension Sequence where Element: BinaryEncodable {
     func toBoolArray() -> [Bool] {
         return map { $0.toBoolArray() }.reduce([], +)
+    }
+}
+
+public extension BinaryEncodable {
+    static func == (lhs: Self, rhs: Self) -> Bool {
+        return lhs.toBoolArray() == rhs.toBoolArray()
     }
 }
