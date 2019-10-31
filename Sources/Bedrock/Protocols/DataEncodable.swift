027a0dde2815495c493214d0748cee40b41a24fe
CAHjbao <pumperknickle@protonmail.com>
2019-10-31 11:19:34 -0500

    dataencodableisbinaryencodable

--- Sources/Bedrock/Protocols/DataEncodable.swift
+++ Sources/Bedrock/Protocols/DataEncodable.swift
@@ -1,6 +1,17 @@
 import Foundation
 
-public protocol DataEncodable {
+public protocol DataEncodable: BinaryEncodable {
 	func toData() -> Data
 	init?(data: Data)
+}
+
+extension DataEncodable {
+	public func toBoolArray() -> [Bool] {
+		return toData().toBoolArray()
+	}
+	
+	public init?(raw: [Bool]) {
+		guard let rawData = Data(raw: raw) else { return nil }
+		self.init(data: rawData)
+	}
 }
