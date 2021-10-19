d5997712b7ee0f088b896509eb8ccd5c18ae4f3d
Pumper Knickle <pumperknickle@protonmail.com>
2021-10-18 23:44:47 -0500

    array slice functionality

---
+++ Sources/Bedrock/Extensions/ArraySliceExtensions.swift
@@ -0,0 +1,58 @@
+import Foundation
+
+infix operator |||
+
+public extension ArraySlice {
+    static func - (left: ArraySlice, right: ArraySlice) -> ArraySlice {
+        return ArraySlice(left.dropFirst(right.count))
+    }
+}
+
+public extension ArraySlice where Element: Equatable {
+    static func ~> (left: ArraySlice, right: ArraySlice) -> ArraySlice {
+        return ArraySlice(left.prefix(left.indexSame(right: right)))
+    }
+
+    func indexSame(right: ArraySlice) -> Int {
+        return indexSame(index: 0, right: right)
+    }
+
+    func indexSame(index: Int, right: ArraySlice) -> Int {
+        if self.count <= index || right.count <= index { return index }
+        return self[index] == right[index] ? indexSame(index: index + 1, right: right) : index
+    }
+}
+
+public extension ArraySlice where Element: Comparable {
+    static func < (lhs: ArraySlice<Element>, rhs: ArraySlice<Element>) -> Bool {
+        guard let firstLeft = lhs.first else { return true }
+        guard let firstRight = rhs.first else { return false }
+        if firstLeft == firstRight { return ArraySlice(lhs.dropFirst()) < ArraySlice(rhs.dropFirst()) }
+        return firstLeft < firstRight
+    }
+}
+
+public extension ArraySlice where Element == Bool {
+    func toBytes() -> [UInt8] {
+        let numBits = count
+        let numBytes = (numBits + 7) / 8
+        var bytes = [UInt8](repeating: 0, count: numBytes)
+        
+        for (index, bit) in enumerated() {
+            if bit == true {
+                bytes[index / 8] += 1 << (7 - index % 8)
+            }
+        }
+        return bytes
+    }
+    
+    func literal() -> String {
+        return map { $0 ? "1" : "0" }.reduce("", +)
+    }
+    
+    static func |||(lhs: ArraySlice<Bool>, rhs: ArraySlice<Bool>) -> ArraySlice<Bool> {
+        guard let firstlhs = lhs.first else { return ArraySlice([]) }
+        guard let firstrhs = rhs.first else { return lhs }
+        return ArraySlice([firstlhs != firstrhs]) + (lhs.dropFirst() ||| rhs.dropFirst())
+    }
+}
